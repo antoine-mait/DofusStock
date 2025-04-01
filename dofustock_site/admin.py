55ea1f79e79d41b3b6cb9192407c6654c77dd1cf
antoine-mait <antoine.maitrias@gmail.com>
2025-04-01 15:34:03 +0200

    change Admin panel to correctly see user craft list and recipes description and effect on items

--- dofustock_site/admin.py
+++ dofustock_site/admin.py
@@ -1,34 +1,75 @@
 from django.contrib import admin
-from .models import User, Item, Effect , Recipe , Craftlist
+from django.db import models
+from django.contrib.admin import widgets
+from .models import User, Item, Effect, Recipe, Craftlist
+from django.urls import reverse
+from django.utils.html import format_html
+from django.contrib.auth.admin import UserAdmin
 
-# Register User model
-admin.site.register(User)
-admin.site.register(Item)
-admin.site.register(Effect)
-admin.site.register(Recipe)
-# Register your models here.
+# First, register the standard inlines and admin classes
 
+class EffectInline(admin.TabularInline):
+    model = Effect
+    extra = 1
+    readonly_fields = ['description']
+    fields = ['description']
 
-# Customized admin for Craftlist
+class RecipeInline(admin.TabularInline):
+    model = Recipe
+    extra = 1
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'ankama_id', 'category', 'item_type', 'level']
+    search_fields = ['name', 'ankama_id']
+    list_filter = ['category', 'item_type', 'level', 'is_weapon']
+    readonly_fields = ['description_display']
+    fieldsets = [
+        (None, {'fields': ['ankama_id', 'name', 'category', 'item_type', 'level', 'is_weapon', 'image_url']}),
+        ('Description', {'fields': ['description_display']}),
+    ]
+    inlines = [EffectInline, RecipeInline]
+    
+    def description_display(self, obj):
+        return obj.description
+    description_display.short_description = 'Item Description'
+
 class CraftlistAdmin(admin.ModelAdmin):
     list_display = ['user', 'get_items']
-    filter_horizontal = ['item']  # This creates a better interface for the M2M relationship
+    filter_horizontal = ['item']
     
     def get_items(self, obj):
         return ", ".join([item.name for item in obj.item.all()])
     
     get_items.short_description = 'Items in Craftlist'
     
-    # If you want to add search functionality to the items selection
     search_fields = ['user__username']
-    autocomplete_fields = ['item']  # Enables autocomplete for the item field
+    autocomplete_fields = ['item']
+
+# Now handle the User admin customization
+
+class CraftlistInline(admin.StackedInline):
+    model = Craftlist
+    filter_horizontal = ['item']
+    can_delete = False
+    verbose_name_plural = 'Craftlist'
+
+class CustomUserAdmin(UserAdmin):
+    inlines = UserAdmin.inlines + (CraftlistInline,)
+    list_display = UserAdmin.list_display + ('view_craftlist',)
     
+    def view_craftlist(self, obj):
+        try:
+            craftlist = Craftlist.objects.get(user=obj)
+            url = reverse('admin:dofustock_site_craftlist_change', args=[craftlist.id])
+            return format_html('<a href="{}">View Craftlist</a>', url)
+        except Craftlist.DoesNotExist:
+            return format_html('<span>No Craftlist</span>')
+        
+    view_craftlist.short_description = 'Craftlist'
+
+# Register all models
+admin.site.register(Item, ItemAdmin)
 admin.site.register(Craftlist, CraftlistAdmin)
 
-# To enable autocomplete on the Item model
-class ItemAdmin(admin.ModelAdmin):
-    search_fields = ['name', 'ankama_id']
-
-# Re-register Item with the custom admin
-admin.site.unregister(Item)
-admin.site.register(Item, ItemAdmin)
+# Register User with custom admin - instead of unregistering and re-registering
+admin.site.register(User, CustomUserAdmin)
